b8caf9ba7c5bfb1aad2917d532baaee907be0f15
OrieAYA <113887186+OrieAYA@users.noreply.github.com>
2023-03-08 19:37:03 +0100

    Multiply Matrices function

--- Matrice.py
+++ Matrice.py
@@ -1,23 +1,10 @@
 
 def multMat(matA, matB):
     if verFormat(matA, matB): return False
-    matReturn = [[value for value in [column for column in range(len(matA[0]))]] for ligne in range(len(matA))]
-
-    return matReturn
-
-    'matA[value][ligne] * matB[column][value]'
-
-    matRetour = []
-    for i in range(len(matA)):
-        matPassage = []
-        for j in range(len(matA[0])):
-            for index in range(len(matA[0])):
-                sum += matA[index][i] * matB[j][index]
-            matPassage += [sum]
-        matRetour += [matPassage]
+    return [[sum([i for i in [matA[value][ligne] * matB[column][value] for column in range(len(matA[0]))]]) for value in range(len(matA[0]))] for ligne in range(len(matA))]
 
 def verFormat(matA, matB):
-    return True
+    return False
 
 matA = [
     [1,1,1],
