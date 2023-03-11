c9437c21f9fff54afad5e7c5e35517128bd57896
OrieAYA <113887186+OrieAYA@users.noreply.github.com>
2023-03-11 02:32:10 +0100

    Multiply by a Scal Matrices function

--- Matrice.py
+++ Matrice.py
@@ -3,6 +3,10 @@ def multMat(matA, matB):
     if verFormat(matA, matB) or len(matA[0]) != len(matB): return False
     return [[sum([i for i in [matA[value][ligne] * matB[column][value] for column in range(len(matA[0]))]]) for value in range(len(matA[0]))] for ligne in range(len(matA))]
 
+def mutlScalMat(matA, k):
+    if verFormatMatUnit(matA): return False
+    return [[matA[ligne][column] * k for column in range(len(matA[0]))] for ligne in range(len(matA))]
+
 def addMat(matA, matB):
     if verFormat(matA, matB) or len(matA[0]) != len(matB[0]) or len(matA) != len(matB): return False
     return [[matA[ligne][column] + matB[ligne][column] for column in range(len(matA[0]))] for ligne in range(len(matA))]
@@ -12,6 +16,9 @@ def soustMat(matA, matB):
     return [[matA[ligne][column] - matB[ligne][column] for column in range(len(matA[0]))] for ligne in range(len(matA))]
 
 def verFormat(matA, matB):
+    return False
+
+def verFormatMatUnit(matA):
     return False
 
 matA = [
