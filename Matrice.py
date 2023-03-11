ec40af42da695f42ed23c8970e3e3761e9631318
OrieAYA <113887186+OrieAYA@users.noreply.github.com>
2023-03-11 02:24:13 +0100

    Additionner and soustraire Matrices function

--- Matrice.py
+++ Matrice.py
@@ -1,7 +1,15 @@
 
 def multMat(matA, matB):
-    if verFormat(matA, matB): return False
+    if verFormat(matA, matB) or len(matA[0]) != len(matB): return False
     return [[sum([i for i in [matA[value][ligne] * matB[column][value] for column in range(len(matA[0]))]]) for value in range(len(matA[0]))] for ligne in range(len(matA))]
+
+def addMat(matA, matB):
+    if verFormat(matA, matB) or len(matA[0]) != len(matB[0]) or len(matA) != len(matB): return False
+    return [[matA[ligne][column] + matB[ligne][column] for column in range(len(matA[0]))] for ligne in range(len(matA))]
+
+def soustMat(matA, matB):
+    if verFormat(matA, matB) or len(matA[0]) != len(matB[0]) or len(matA) != len(matB): return False
+    return [[matA[ligne][column] - matB[ligne][column] for column in range(len(matA[0]))] for ligne in range(len(matA))]
 
 def verFormat(matA, matB):
     return False
@@ -17,5 +25,3 @@ matB = [
     [1,1,1],
     [1,1,1],
 ]
-
-print(multMat(matA, matB))
